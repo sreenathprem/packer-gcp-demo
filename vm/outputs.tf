31a0f4fe3ccf14e1d5b04c6bd71bb31719686653
sreenathprem <sreenath.premnadh@hashicorp.com>
2024-07-08 23:24:46 +0200

    Fixing the outputs

--- vm/outputs.tf
+++ vm/outputs.tf
@@ -2,6 +2,6 @@ output "ubuntu_version" {
   value = data.hcp_packer_version.ubuntu
 }
 
-output "ubuntu_us_east_2" {
-  value = data.hcp_packer_artifact.ubuntu_us_east_2
+output "ubuntu_europe_west3" {
+  value = data.hcp_packer_artifact.ubuntu_europe_west3
 }
